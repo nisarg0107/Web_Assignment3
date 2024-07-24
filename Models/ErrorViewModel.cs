af02a6501fdec6a5170a3f7cba9426ac6cd96cba
nisarg0107 <nisarg172001@gmail.com>
2024-07-24 16:20:56 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Assignment3_NisargPatel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
